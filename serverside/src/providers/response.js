9a85a13fd642acd2c1ae35cab704b67cdae61a92
u4bi <admin@naver.id>
2018-06-27 02:04:18 +0900

    Added response headers.

--- serverside/src/providers/response.js
+++ serverside/src/providers/response.js
@@ -1,7 +1,7 @@
 export const response = (statusCode, body) => ({
     statusCode: statusCode,
-    body: body,
-    // headers: { "Access-Control-Allow-Origin": "*", "Access-Control-Allow-Credentials": true }
+    body: JSON.stringify(body),
+    headers: { 'Content-Type' : 'application/json', 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Credentials': true }
 });
 export const success = body => response(200, body);
 export const failed = body => response(500, body);
